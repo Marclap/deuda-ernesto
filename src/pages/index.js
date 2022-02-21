6b931c7b4d0b9d101b4de09e05ba5eb6bd5f07c7
Marclap <jhona.cb991128@gmail.com>
2022-02-21 11:34:20 -0500

    Primera version

---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import Head from 'next/head'
+import Card from './components/Card'
+import Historial from './components/Historial'
+
+export default function Home() {
+    let valores = {
+        deuda1: 800000,
+        deuda2: 600000,
+    }
+    return (
+        <>
+            <Head>
+                <title>Deuda: Ernesto Montes</title>
+            </Head>
+            <section className="bg-fondo pt-20 pb-12 relative z-20 overflow-hidden h-screen">
+                <div className="container mx-auto">
+                    <div className="flex flex-wrap -mx-4">
+                        <div className="w-full px-4">
+                            <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
+                                <span className="font-semibold text-lg text-primary mb-2 block">
+                                    Ernesto Montes
+                                </span>
+                                <h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
+                                    Nuestra deuda
+                                </h2>
+                                <p className="text-base text-body-color">
+                                    Aquí se podrá ver como va el avance de la
+                                    deuda, tanto los intereses y los pagos de
+                                    esta.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row">
+                        <Card
+                            titulo={'Deuda 1'}
+                            precio={valores.deuda1}
+                            fecha={'28/01/2022'}
+                            fechaUltimoPago={''}
+                            valorUltimoPago={''}
+                        />
+                        <Card
+                            titulo={'Deuda 2'}
+                            precio={valores.deuda2}
+                            fecha={'8/02/2022'}
+                            fechaUltimoPago={''}
+                            valorUltimoPago={''}
+                        />
+                        <Card
+                            titulo={'Deuda total'}
+                            precio={valores.deuda1 + valores.deuda2}
+                            fecha={''}
+                            fechaUltimoPago={''}
+                            valorUltimoPago={''}
+                        />
+                    </div>
+                </div>
+                <div className="container mx-auto">
+                    <Historial />
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                fondo: '#8E806A',
+                fondoSecundario: '#748CAB',
+                colorBorde: '#3E5C76',
+                colorLinea: '#1D2D44',
+            },
+        },
+    },
+    plugins: [],
+}
