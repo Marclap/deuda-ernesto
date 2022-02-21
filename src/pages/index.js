b05e2b8a940c0e2d728b485992b3c48af52c31bc
Marclap <jhona.cb991128@gmail.com>
2022-02-21 12:03:22 -0500

    Cambio de color

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ export default function Home() {
             <Head>
                 <title>Deuda: Ernesto Montes</title>
             </Head>
-            <section className="bg-fondo pt-20 pb-12 relative z-20 overflow-hidden h-screen">
+            <section className="relative z-20 h-screen pt-20 pb-12 overflow-hidden bg-colorLinea">
                 <div className="container mx-auto">
-                    <div className="flex flex-wrap -mx-4">
+                    <div className="flex flex-wrap -mx-4 text-white">
                         <div className="w-full px-4">
                             <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-                                <span className="font-semibold text-lg text-primary mb-2 block">
+                                <span className="block mb-2 text-lg font-semibold text-primary">
                                     Ernesto Montes
                                 </span>
                                 <h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
